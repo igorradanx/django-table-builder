5249afbd63174e1539240b781a53667fa83f15d5
igorradanx <igor.radan.bix@gmail.com>
2023-05-13 11:00:34 +0200

    ;feat: docker-compose configure

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Table builder app
 
-## Installation
+## Using docker compose
+- Run project by `docker compose up`
+- Check postman collection and try
+
+## Manual Installation
 - Install dependencies: `pip install -r requirements.txt`
 - Set database credentials in table_builder/settings.py
 - Run web server: `python manage.py runserver 8080`
--- table_builder/settings.py
+++ table_builder/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +79,10 @@ WSGI_APPLICATION = 'table_builder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": "table_builder",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "192.168.209.128",
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
         "PORT": "5432",
     }
 }
